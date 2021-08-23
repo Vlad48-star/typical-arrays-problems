de20b1de8fbd0ab158066979b967973576589f57
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2021-08-23 17:08:11 +0300

    fix:return null

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array || array.length == 0){
+    return 0
+  }
+  // let min = array && array.length ? array[0] : 0;
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (array[i] < min) {
+  //       min = array[i];
+  //     }
+  //   }
+    return Math.min(...array);
+  }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0
+  }
+  let sum = array.reduce((a, b) => a + b) / array.length;
+  return sum
 }
